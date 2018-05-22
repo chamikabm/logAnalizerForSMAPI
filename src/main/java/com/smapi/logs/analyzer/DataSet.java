8c5a41cda8540b14a752bd221456bba115f8657a
chamikabm <kasun.chamika@gmail.com>
2018-05-22 21:28:10 +0530

    Add map values

--- src/main/java/com/smapi/logs/analyzer/DataSet.java
+++ src/main/java/com/smapi/logs/analyzer/DataSet.java
@@ -13,12 +13,12 @@ class DataSet {
         DataConfigurer dataConfigurer = new DataConfigurer();
         dataConfigurer.configureInitialDataSet();
 
-        servicesMap.put("Student", 6);
-        servicesMap.put("Lecturer", 6);
+        servicesMap.put("Student", 12);
+        servicesMap.put("Lecturer", 8);
         servicesMap.put("Department", 6);
         servicesMap.put("Payment", 6);
-        servicesMap.put("Exam", 6);
-        servicesMap.put("Registration", 6);
+        servicesMap.put("Exam", 7);
+        servicesMap.put("Registration", 10);
 
         reposMap.put("Student", 6);
         reposMap.put("Lecturer", 6);
@@ -29,9 +29,9 @@ class DataSet {
 
         managersMap.put("Student", 6);
         managersMap.put("Lecturer", 6);
-        managersMap.put("Department", 6);
-        managersMap.put("Payment", 6);
-        managersMap.put("Exam", 6);
-        managersMap.put("Registration", 6);
+        managersMap.put("Department", 5);
+        managersMap.put("Payment", 4);
+        managersMap.put("Exam", 4);
+        managersMap.put("Registration", 7);
     }
 }
